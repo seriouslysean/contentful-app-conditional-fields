d9b401b2f34a2a9c9d11c222a3287fd9e414b7fe
Sean Kennedy <sean@codeserious.com>
2022-08-08 11:20:44 -0400

    Conditionally hide/show fields in the Product Highlight Model

--- src/locations/EntryEditor.tsx
+++ src/locations/EntryEditor.tsx
@@ -1,25 +1,21 @@
+// @ts-nocheck
+
 import { useState, useEffect } from 'react';
 import { EditorExtensionSDK } from '@contentful/app-sdk';
 import { Field, FieldWrapper } from "@contentful/default-field-editors";
+import { FormControl, Checkbox } from "@contentful/f36-components";
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 
-// Prop types for DefaultField component
-interface DefaultFieldProps {
-  fieldId: string;
-  sdk: any;
-  widgetId: string | null;
-}
-
 // Converts a field into <FieldAPI> data type, which is the expected data type for many API methods
-const getFieldAPI = (fieldId: string, sdk: EditorExtensionSDK) =>
+const getFieldAPI = (fieldId, sdk) =>
   sdk.entry.fields[fieldId].getForLocale(sdk.locales.default);
 
 // Creates a <FieldExtensionSDK> type that can be passed to components from the default-field-editors package
-const getFieldExtensionSdk = (fieldId: string, sdk: EditorExtensionSDK) =>
+const getFieldExtensionSdk = (fieldId, sdk) =>
   Object.assign({ field: getFieldAPI(fieldId, sdk) }, sdk);
 
 // Render default contentful fields using Forma 36 Component
-const DefaultField = (props: DefaultFieldProps) => {
+const DefaultField = (props) => {
   const { fieldId, sdk, widgetId } = props;
 
   return (
@@ -29,47 +25,114 @@ const DefaultField = (props: DefaultFieldProps) => {
   );
 };
 
+// A stateful Boolean component that mimics Contentful's default
+const CustomBoolean = (props) => {
+  const { field, handleChange, sdk, value } = props;
+  return (
+    <FieldWrapper sdk={sdk} name={field.name}>
+      <FormControl isRequired>
+        <Checkbox
+          name={`${field.id}-controlled`}
+          id={`${field.id}-controlled`}
+          isChecked={value}
+          onChange={(e) => handleChange(field.id, e.target.checked)}
+        ></Checkbox>
+      </FormControl>
+    </FieldWrapper>
+  );
+};
+
 const Entry = () => {
-  const sdk = useSDK<EditorExtensionSDK>();
+  const sdk = useSDK();
   const locale = sdk.locales.default;
   const { fields } = sdk.contentType;
 
   console.log('!!! sdk', sdk);
 
-  // Initial state values
-  const [isPrimary, setIsPrimary] = useState(false);
+  // Store the input values of managed fields.
+  // Managed fields are fields whose state needs to be managed by this app.
+  const [managedFieldValues, setManagedFieldValues] = useState(
+    fields
+      .map((field) => field.id)
+      .reduce((acc, field) => {
+        const defaultValueObject = fields.find(
+          (editorField) => editorField.id === field
+        )?.defaultValue;
+        const defaultValue = defaultValueObject
+          ? defaultValueObject[locale]
+          : '';
+        return { ...acc, [field]: defaultValue };
+      }, {})
+  );
+
+  // save input to Contentful
+  const saveInput = (fieldId, input) => {
+    getFieldAPI(fieldId, sdk)
+      .setValue(input)
+      .then((data) => {
+        console.log(`saving data to contentful... ${fieldId}:  ${data}`);
+      })
+      .catch((err) => {
+        console.log("something went wrong with saving data to contentful.");
+        console.log(err);
+      });
+  };
+
+  // Update state value for a managed field
+  const updateInput = (fieldId, isChecked) => {
+    setManagedFieldValues((prevState) => ({
+      ...prevState,
+      [fieldId]: isChecked,
+    }));
+
+    // Save data to contentful after 3 seconds
+    setTimeout(() => {
+      saveInput(fieldId, isChecked);
+    }, 3000);
+  };
 
   useEffect(() => {
-    console.log('??? isPrimary', isPrimary);
-  });
+    // ???
+  }, [managedFieldValues, fields]);
 
   const fieldComponents = fields.map((field) => {
     const control = sdk.editor.editorInterface.controls!.find(
       (control) => control.fieldId === field.id
     );
     const widgetId = control?.widgetId || null;
+    const defaultValue = field.defaultValue?.hasOwnProperty(locale) ?
+      field.defaultValue[sdk.locales.default] : null;
+    const fieldsToToggle = ['image', 'icon', 'bodyText'];
 
-    // If this field is a the conditional toggle, add a listener to the value
-    if (field.name === 'isPrimary') {
-      const elementsToToggle = ['image', 'icon', 'bodyText'];
-      const partialFieldId = `entity.${field.id}.${locale}.${field.required}.`;
-      const isPrimaryEl = (
-        document.querySelector(`input[id^="${partialFieldId}"]`) as HTMLInputElement
+    if (field.id === 'isPrimary') {
+      return (
+        <div className="c-cf-field" key={field.id}>
+          <CustomBoolean
+            value={
+              // TODO: How do we get the initial value? The default value isn't updated
+              // to the set value from the initial contentful response and I don't see it in the
+              // sdk object
+              managedFieldValues[field.id] || defaultValue
+            }
+            field={field}
+            handleChange={updateInput}
+            sdk={getFieldExtensionSdk(field.id, sdk)} />
+          </div>
       );
-      console.log('!!! partialFieldId', partialFieldId);
-      console.log('!!! inputEl', isPrimaryEl);
-      // set default state value
-      // setIsPrimary(isPrimaryValue === 'true')
-      // const currentValue = document.querySelector(`#${fieldId}`)?.value;
     }
 
+    const getInlineStyle = (fieldId) => {
+      return managedFieldValues['isPrimary'] === true && fieldsToToggle.includes(fieldId) ?
+        { display: 'none' } : {};
+    };
     return (
-      <DefaultField
-          key={field.id}
+      <div className="c-cf-field" style={getInlineStyle(field.id)} key={field.id}>
+        <DefaultField
           fieldId={field.id}
           sdk={getFieldExtensionSdk(field.id, sdk)}
           widgetId={widgetId}
         />
+      </div>
     );
   })
 
